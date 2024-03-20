ec1f746e7f4e7e65861e76db00acca0ec876d039
MarcoGotti <mrc.gotti@gmail.com>
2024-03-20 16:10:36 +0000

    refa

--- database/db.php
+++ database/db.php
@@ -1,6 +1,6 @@
 <?php
 
-include __DIR__ . '/../models/models.php'; //
+include __DIR__ . '/../models/classMovie.php'; //
 
 $movies =
     [
@@ -13,10 +13,10 @@ $movies =
     ];
 
 //read from models.php
-var_dump($first_movie->normalFunct());
+//var_dump($first_movie->normalFunct());
 
 
 
-var_dump($movies);
+//var_dump($movies);
 
 //array_push($movies, $first_movie, $second_movie, $third_movie, $fourth_movie, $fifth_movie);
